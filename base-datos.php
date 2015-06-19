7c29e8757829fc44f7b975e70e73058a61aef996
aaronesteban <ennety@gmail.com>
2015-06-19 16:57:41 +0200

    first commit

---
+++ base-datos.php
@@ -0,0 +1,55 @@
+<?php
+
+//CLASES
+
+class blog
+{
+	public $id;
+	public $titulo;
+	public $subtitulo;
+	public $texto;
+	public $fecha;
+	public $bd;
+
+	function __construct()
+	{
+
+		//CONECTARSE A LA BASE DE DATOS
+
+		$this->bd = new mysqli('localhost', 'root', '4ndr3s', 'blog-prueba');
+
+		if($this->bd->connect_errno > 0) 
+		{
+			die('Imposible conectar [' . $this->bd->connect_error . ']');
+		}
+
+	}
+
+	public function listar()
+	{
+		$sql = "SELECT * FROM posts";
+
+		if (!$resultado = $this->bd->query($sql)) 
+		{
+			die ('Ocurrio un error ejecutando el query [' . $bd->error .']');
+		}
+		return $resultado;
+
+	}
+
+
+
+	public function view($id=null)
+	{
+		$sql = "SELECT * FROM posts WHERE id=$id";
+
+		if (!$resultado = $this->bd->query($sql)) 
+		{
+			die ('Ocurrio un error ejecutando el query [' . $bd->error .']');
+		}
+		return $resultado;
+
+	}
+}
+
+
---
+++ view.php
@@ -0,0 +1,46 @@
+<?php
+	include	"base-datos.php";
+
+	$temp = new blog();
+	$posts = $temp->view($_GET["id"]);
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+	    <meta charset="utf-8">
+	    <meta name="BLOG" content="width=device-width, initial-scale=1.0">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+
+	    <title>Blog en php</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+		<div class="container">
+
+			<h1>Blog</h1>
+
+			<div class="row">
+
+				<div class="col-sm-8 blog-main">
+
+					<?php 
+						while ($fila = $posts->fetch_assoc()): ?>
+							<div class="blog-post">
+								<h2 class="blog-post-title">
+									<?=$fila['titulo']?>
+								</h2>
+								<p class="blog-post-meta"><?=$fila['fecha']?></p>
+								<h3 class=""><?=$fila['subtitulo']?></h3>
+								<p><?=$fila['texto']?></p>
+							</div>
+						<?php endwhile; ?>
+				</div>
+			</div>
+	</body>
+</html>
